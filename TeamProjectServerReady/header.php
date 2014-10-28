e04073a2f5dd53ad9e0404cbc8ebc744ab779829
odschulz <grg.marchev@gmail.com>
2014-10-28 11:10:04 +0200

    adsjustments to php files

--- TeamProjectServerReady/header.php
+++ TeamProjectServerReady/header.php
@@ -3,19 +3,19 @@
 <header>
 	<div id="headerContainer">
 		<div id="header">
-		    <a href="home.php">
+		    <a href="home.html">
 		        <img src="Images/Logos/rsz_finallogo.png" alt="Logo"/>
 		        <span>GEEK YOURSELF</span>
 		    </a>
 		</div>
 		<nav id="headerMenu">
 		    <ul class="list-inline list-unstyled">
-		        <li><a href="home.php">Home</a></li>
-		        <li><a href="about.php">About</a></li>
-		        <li><a href="geekhq.php">Geek HQ</a></li>
-		        <li><a href="gallery.php">Gallery</a></li>
-		        <li><a href="jokes.php">Jokes</a></li>
-		        <li><a href="contacts.php">Contact</a></li>
+		        <li><a href="home.html">Home</a></li>
+		        <li><a href="about.html">About</a></li>
+		        <li><a href="geekhq.html">Geek HQ</a></li>
+		        <li><a href="gallery.html">Gallery</a></li>
+		        <li><a href="jokes.html">Jokes</a></li>
+		        <li><a href="contacts.html">Contact</a></li>
 		    </ul>
 		</nav>
 	</div>
