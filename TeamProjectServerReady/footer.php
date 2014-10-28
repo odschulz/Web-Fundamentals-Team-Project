d78d1c204157336fa7d255f474c5e64357a004e3
odschulz <grg.marchev@gmail.com>
2014-10-28 10:40:52 +0200

    final adjustments and added server-ready folder

---
+++ TeamProjectServerReady/footer.php
@@ -0,0 +1,54 @@
+<footer>
+    <div id="footerWrapper">
+        <div id="footerContainer">
+            <div id="footerContent">
+
+                <div class="col-md-1"></div>
+                <div id="footerLinks" class="col-xs-6 col-md-5 col-lg-5">
+                    <h3>Useful Links</h3>
+                    <ul>
+                        <li>
+                            <a href="https://softuni.bg/" id="softuniLink" target="_blank"></a>
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/channel/UCPlL0QmMNVtubUKO2SqU4zQ"
+                               id="youtubeLink" target="_blank"></a>
+                        </li>
+                        <li>
+                            <a href="https://www.facebook.com/SoftwareUniversity"
+                               id="facebookLink" target="_blank"></a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <a href="http://validator.w3.org/check?uri=referer" target="_blank">
+                                <img class="validators"
+                                     src="Images/Validators/html5.png"
+                                     alt="Valid HTML5!" />
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank">
+                                <img class="validators"
+                                     src="Images/Validators/css.gif"
+                                     alt="Valid CSS!" />
+                            </a>
+                        </li>
+
+                    </ul>
+                </div>
+
+                <div id="disclaimer" class="col-xs-6 col-md-5 col-lg-5 text-right">
+                    <p>Copyright © 2014 <br/>
+                    <em>Geek Yourself</em><br/>
+                    A student project by<br/>
+                    <em>Team Floral White</em> <br/>
+                    All rights reserved</p>
+                </div>
+                <div class="col-md-1"></div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+
---
+++ TeamProjectServerReady/header.php
@@ -0,0 +1,22 @@
+
+
+<header>
+	<div id="headerContainer">
+		<div id="header">
+		    <a href="home.php">
+		        <img src="Images/Logos/rsz_finallogo.png" alt="Logo"/>
+		        <span>GEEK YOURSELF</span>
+		    </a>
+		</div>
+		<nav id="headerMenu">
+		    <ul class="list-inline list-unstyled">
+		        <li><a href="home.php">Home</a></li>
+		        <li><a href="about.php">About</a></li>
+		        <li><a href="geekhq.php">Geek HQ</a></li>
+		        <li><a href="gallery.php">Gallery</a></li>
+		        <li><a href="jokes.php">Jokes</a></li>
+		        <li><a href="contacts.php">Contact</a></li>
+		    </ul>
+		</nav>
+	</div>
+</header>
